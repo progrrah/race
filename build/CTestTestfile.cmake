1075d80c5c70c04d773e00a5da7efeaae18a4712
progrrah <mojang883@gmail.com>
2024-05-18 15:23:06 +0300

    aaaa

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/raxim4uk/Projects/race
-# Build directory: /home/raxim4uk/Projects/race/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
